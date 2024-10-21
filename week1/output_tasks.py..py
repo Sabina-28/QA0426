a86ad7b9a3b6d2a73da8fd888813948e476d96a7
Sabina-28 <0manes61@solent.ac.uk>
2024-10-21 19:10:32 +0100

    Added code to display escape characters

--- week1/output_tasks.py..py
+++ week1/output_tasks.py..py
@@ -1,9 +1,6 @@
-#Display message to the screen
-print("Welcome to QH0426 !")
-print("In week 1 we will learn...")
-
-print("...How to use Git and GitHub")
-print("...How to output to the screen")
-print("...How to get user input")
-
-print("I hope you are enjoying the lesson thus far!")
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
