6aba2d37402d03ec87b91ebe3762862ad9345371
Sabina-28 <0manes61@solent.ac.uk>
2024-10-28 18:45:51 +0000

    Added code to display an ASCII robot with dynamic eyes

--- week1/input_tasks.py
+++ week1/input_tasks.py
@@ -1,11 +1,11 @@
-# Display a box
-print("##########")
-print("#        #")
-print("#        #")
-print("##########")
-#Ask user to enter a character
-print("Please enter a character for the eye")
-character = input()
-print("###########")
-print(f"# {character}{character}")
-print("###########")
+# Ask user to enter their name
+name = input("What is your name? ")
+# Ask user to enter their age
+print("How old are you (in years)?")
+age = int(input())
+#Ask user their height
+print("How tall are you (in meters)?")
+height = float(input())
+# Ask user their weight
+print("How much do you weigh (in kilograms)?")
+weight = float(input())
