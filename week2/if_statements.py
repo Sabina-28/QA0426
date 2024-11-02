c62e1a9f2d72860551bfba2a02d1a4b9e959ab66
Sabina-28 <0manes61@solent.ac.uk>
2024-11-02 13:07:22 +0000

    Added code to demonstrate how a variable can be used as a counter

--- week2/if_statements.py
+++ week2/if_statements.py
@@ -1,11 +1,29 @@
 #Ask user to enter numbers
-print("Please enter the first number")
+print("Please enter the first whole number")
 number1 = int(input())
-print("Please enter the second number")
+print("Please enter the second whole number")
 number2 = int(input())
-if number1 > number2:
-    print("The first number is greater than the second number")
-elif number1 < number2:
-    print("The second number is greater than the first number")
+print("Please enter the third whole number")
+number3 = int(input())
+
+even_numbers = 0
+odd_numbers = 0
+
+# Determine which numbers are even and which are odd
+if number1 % 2 == 0:
+    even_numbers = even_numbers + 1
 else:
-    print("The numbers are equal")
+    odd_numbers = odd_numbers + 1
+
+if number2 % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+
+if number3 % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+
+# Display result
+print(f"There were {even_numbers} even and {odd_numbers} odd numbers.")
