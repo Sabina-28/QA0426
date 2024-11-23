d7d6880d8f1b0d4432b8168b21c1696e0b14f784
Sabina-28 <0manes61@solent.ac.uk>
2024-11-23 12:55:53 +0000

    simple for loop

---
+++ week3 /for_loops.py
@@ -0,0 +1,12 @@
+# Ask user input
+print("How many mountains should I display")
+answer = int(input())
+
+# Use a for loop to display
+
+print("Displaying...")
+for i in range(answer):
+    print("     /\ ")
+    print("     ~~")
+    print("    /  \ ")
+    print("   /    \ ")
