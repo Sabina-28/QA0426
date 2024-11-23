1336efcf5d7a4b7083686b70e8b0c0e86f96ca65
Sabina-28 <0manes61@solent.ac.uk>
2024-11-23 12:22:18 +0000

    use of summing with a while loop

--- week3 /while_loops.py
+++ week3 /while_loops.py
@@ -1,7 +1,10 @@
-# Ask user a phrase
-print("Please enter a phrase")
-phrase = input()
-# Calculate the number of characters in phrase
-num_characters = len(phrase)
-# Display "Hi" for each character in the phrase
-print("Hi " * num_characters)
+# Calculate the sum of the first 100 numbers
+print("Calculating the sum of the first 100 numbers...")
+# Declare variables
+sum_of_numbers = 0
+counter = 1
+while counter <= 100:
+    sum_of_numbers += counter
+    counter += 1
+# Display result
+print(f"...Done! The answer is {sum_of_numbers}")
