8f0958e9f5012cbc08936d8b68f70ca553a7b724
Sabina-28 <0manes61@solent.ac.uk>
2024-11-23 12:04:49 +0000

    use of a while loop, len function

--- week3 /while_loops.py
+++ week3 /while_loops.py
@@ -1,15 +1,7 @@
-# Ask user for number of bars
-print("How many bars should be charged?")
-bars_to_charge = int(input())
-
-# Declare a control variable
-bars_charged = 0
-
-# Display bars
-print()
-
-while bars_charged < bars_to_charge:
-    bars_charged = bars_charged + 1
-    print(f"Charging: {'█ ' * bars_charged}")
-
-print("The battery is fully charged.")
+# Ask user a phrase
+print("Please enter a phrase")
+phrase = input()
+# Calculate the number of characters in phrase
+num_characters = len(phrase)
+# Display "Hi" for each character in the phrase
+print("Hi " * num_characters)
