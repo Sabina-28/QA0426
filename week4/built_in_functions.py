030b5f64b97511a58a3b4f2aa1ccbdad7f0a58e6
Sabina-28 <0manes61@solent.ac.uk>
2024-12-02 21:30:39 +0000

    built-in functions: abs, range, chr

---
+++ week4/built_in_functions.py
@@ -0,0 +1,12 @@
+#Display message
+print("Program started!")
+
+print("Please enter an ASCII code:")
+code = int(input())
+
+if code >= 32 and code <= 126:
+    print(f"The character represented by the ASCII value {code} is: {chr(code)}")
+else:
+    print("The character cannot be displayed.")
+
+print("Program Ended!")
